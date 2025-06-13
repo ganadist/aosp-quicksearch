44e0ccf8bce36a550596e0d83801d19ca35727fc
YOUNG HO CHA <ganachoco@linecorp.com>
2025-06-13 23:23:37 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+
+    val androidGradlePluginVersion: String by settings
+    plugins {
+        id("com.android.settings") version androidGradlePluginVersion
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            val androidGradlePluginVersion: String by settings
+            version("android-gradle", androidGradlePluginVersion)
+        }
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.settings")
+}
+
+android {
+    minSdk = 29
+    compileSdk = 36
+}
+
+rootProject.name = "aosp-quicksearch"
